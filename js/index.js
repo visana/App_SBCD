1cee419896e854fdbabbc2047892f3f0ffbf5b72
visana <ti@visanacomunicacao.com.br>
2019-04-22 15:29:20 -0300

    Implatação OneSignal

--- js/index.js
+++ js/index.js
@@ -47,3 +47,20 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("e4d4f380-a08d-4cf8-8310-0f8246f4b81f")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
